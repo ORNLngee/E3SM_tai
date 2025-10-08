fmyuan/E3SM

Tidal processes added by Sophie LaFond-Hudson and Ben Sulman, ORNL. (Squeezing all commits for easier rebase, -Fengming Yuan).
Add tide data input file with height and salinity to CPL_BYPASS

set marsh salinity = tide channel salinity

associated salinity variable in submodule Infiltration (SoilHydrologyMod)

Move hard-coded tidal cycle parameters to pft_varcon params

added sal_threshold and KM_salinity to pftvarcon

Added sal_threshold and KM_salinity to VegPropType

fixed sal_threshold in pftvarcon and vegetationpropertiestype

added a salinity function to PhotosynthesisMod, a couple notes on SoilStressMod

Added sal_tide as a tidal cycle for salinity

Redefined salinity in SoilHydrologyMod and added qflx_tide and salinity as model outputs

fixed error with qflx_tide in ColumnDataType

minor syntax formatting in photosynthesis & soil hydrology

Adjusting salinity in SoilHydrologyMod

Made salinity proportional to qflx_lat_aqu instead of qflx_tide

trying to fix salinity function in soil hydrology

changes to make salinity modify btran

another attempt at salinity controlling btran

added osm_inhib function to btran in hydraulic stress subroutine, commented out other btran functions

photosynthesis btran updates

edits to photosynthesis mod: add salinity variables to hydraulic stress subroutine

corrected syntax for salinity array

changed sal_threshold & KM_salinity to (veg_pp%itype) instead of (p)

move btran function out of PhotosynthesisMod into CanopyFluxesMod

Added osm_inhib as a variable. Removed salinity function in CanopyFluxes bc it broke carbon balance, moved function to Photosynthesis and multiplied btran by osm_inhib each time bbb was defined.

moved btran function out of CanopyFluxes, in PhotosynthesisMod defined osm_inhib as a variable and multiplied bbbopt by btran and osm_inhib. SoilHydrologyMod has salinity cycle

modify salinity cycle in Soil Hydrology Mod and add osm_inhib as an output variable (not sure yet if it worked)

debugging, trying to

components/elm/src/data_types/ColumnDataType.F90
@@ -108,7 +108,9 @@ module ColumnDataType
     real(r8), pointer :: h2osoi_ice         (:,:) => null() ! ice lens (-nlevsno+1:nlevgrnd) (kg/m2)
     real(r8), pointer :: h2osoi_vol         (:,:) => null() ! volumetric soil water (0<=h2osoi_vol<=watsat) (1:nlevgrnd) (m3/m3)
     real(r8), pointer :: h2osfc             (:)   => null() ! surface water (kg/m2)
-    real(r8), pointer :: salinity           (:)   => null() ! salinity from PFLOTRAN when using interface (TAO 5/19/2020)
+    real(r8), pointer :: salinity           (:) => null() ! salinity from PFLOTRAN when using interface (TAO 5/19/2020)
+    real(r8), pointer :: salt_content       (:,:) => null() ! salt mass for each soil layer
+    real(r8), pointer :: floodf             (:)   => null() ! Flood factor to reduce growth when plants submerged
     real(r8), pointer :: h2ocan             (:)   => null() ! canopy water integrated to column (kg/m2)
     real(r8), pointer :: total_plant_stored_h2o(:)=> null() ! total water in plants (used??)
     real(r8), pointer :: wslake_col         (:)   => null() ! col lake water storage (mm H2O)
@@ -394,6 +396,7 @@ module ColumnDataType
     real(r8), pointer :: eflx_hs_top_snow        (:)   => null() ! heat flux on top snow layer (W/m2)
     real(r8), pointer :: eflx_hs_soil            (:)   => null() ! heat flux on soil [W/m2
     real(r8), pointer :: eflx_sabg_lyr           (:,:) => null() ! absorbed solar radiation (col,lyr) (W/m2)
+    real(r8), pointer :: eflx_sh_tide            (:)   => null() !sensible heat flux from tide
     ! Derivatives of energy fluxes
     real(r8), pointer :: eflx_dhsdT              (:)   => null() ! deriv. of energy flux into surface layer wrt temp (W/m2/K)
     ! Latent heat terms
@@ -1368,13 +1371,15 @@ contains
     !-----------------------------------------------------------------------
     ! allocate for each member of col_ws
     !-----------------------------------------------------------------------
-    allocate(this%h2osoi_liq         (begc:endc,-nlevsno+1:nlevgrnd)) ; this%h2osoi_liq         (:,:) = nan
+    allocate(this%h2osoi_liq         (begc:endc,-nlevsno+1:nlevgrnd)) ; this%h2osoi_liq         (:,:) = nan !SLL 7/28/21
     allocate(this%h2osoi_ice         (begc:endc,-nlevsno+1:nlevgrnd)) ; this%h2osoi_ice         (:,:) = nan
     allocate(this%h2osoi_vol         (begc:endc, 1:nlevgrnd))         ; this%h2osoi_vol         (:,:) = nan
     allocate(this%h2osfc             (begc:endc))                     ; this%h2osfc             (:)   = nan   
     allocate(this%h2ocan             (begc:endc))                     ; this%h2ocan             (:)   = nan 
     allocate(this%wslake_col         (begc:endc))                     ; this%wslake_col         (:)   = nan 
-    allocate(this%salinity           (begc:endc))                     ; this%salinity           (:)   = nan !TAO 5/19/2020
+    allocate(this%salinity           (begc:endc))                     ; this%salinity           (:)   = nan !TAO 5/19/2020 !soil layers SLL 7/13/21
+    allocate(this%salt_content       (begc:endc, 1:nlevgrnd))         ; this%salt_content       (:,:) = nan !SL added 7/27/21
+    allocate(this%floodf             (begc:endc))                     ; this%floodf             (:)   = nan !SL added 8/10/22
     allocate(this%total_plant_stored_h2o(begc:endc))                  ; this%total_plant_stored_h2o(:)= nan  
     allocate(this%h2osoi_liqvol      (begc:endc,-nlevsno+1:nlevgrnd)) ; this%h2osoi_liqvol      (:,:) = nan
     allocate(this%h2osoi_icevol      (begc:endc,-nlevsno+1:nlevgrnd)) ; this%h2osoi_icevol      (:,:) = nan
@@ -1462,6 +1467,27 @@ contains
      call hist_addfld1d (fname='H2OSFC',  units='mm',  &
           avgflag='A', long_name='surface water depth', &
            ptr_col=this%h2osfc)
+
+   !SLL added 4/15/21
+   this%salinity(begc:endc) = spval
+    call hist_addfld1d (fname='SALINITY',  units='ppt', &
+         avgflag='A', long_name='Salinity concentration', &
+         ptr_col=this%salinity)
+
+   this%salt_content(begc:endc,:) = spval
+    call hist_addfld2d (fname='SALT_CONTENT',  units='g', type2d='levgrnd', &
+         avgflag='A', long_name='Mass of salt in soil layer', &
+         ptr_col=this%salt_content)
+   
+   this%floodf(begc:endc) = spval
+    call hist_addfld1d (fname='FLOODF',  units='', &
+    avgflag='A', long_name='Factor 0-1 to reduce plant growth due to flooding', &
+    ptr_col=this%floodf)
+   
+   !this%osm_inhib(begc:endc) = spval
+   ! call hist_addfld1d (fname='OSM_INHIB',  units=' ',  &
+   !      avgflag='A', long_name='Factor to reduce growth due to salinity stress', &
+   !      ptr_col=this%osm_inhib)
 
     this%h2osoi_vol(begc:endc,:) = spval
      call hist_addfld2d (fname='H2OSOI',  units='mm3/mm3', type2d='levgrnd', &
@@ -1602,7 +1628,8 @@ contains
        this%total_plant_stored_h2o(c) = 0._r8
        this%h2osfc(c)                 = 0._r8
        this%h2ocan(c)                 = 0._r8
-       this%salinity(c)               = 0._r8 !TAO added 5/19/2020
+       this%salinity(c)             = 0._r8 !TAO added 5/19/2020
+       this%salt_content(c,:)         = 0._r8
        this%frac_h2osfc(c)            = 0._r8
 
        if (lun_pp%urbpoi(l)) then
@@ -5230,6 +5257,7 @@ contains
     allocate(this%eflx_hs_soil         (begc:endc))              ; this%eflx_hs_soil         (:)   = nan
     allocate(this%eflx_sabg_lyr        (begc:endc, -nlevsno+1:1)); this%eflx_sabg_lyr        (:,:) = nan
     allocate(this%eflx_dhsdT           (begc:endc))              ; this%eflx_dhsdT           (:)   = nan
+    allocate(this%eflx_sh_tide         (begc:endc))              ; this%eflx_sh_tide         (:)   = nan
     allocate(this%htvp                 (begc:endc))              ; this%htvp                 (:)   = nan
     allocate(this%xmf                  (begc:endc))              ; this%xmf                  (:)   = nan
     allocate(this%xmf_h2osfc           (begc:endc))              ; this%xmf_h2osfc           (:)   = nan
@@ -5425,8 +5453,8 @@ contains
     allocate(this%qflx_grnd_irrig        (begc:endc))             ; this%qflx_grnd_irrig      (:)   = nan
     allocate(this%qflx_over_supply       (begc:endc))             ; this%qflx_over_supply     (:)   = nan
     allocate(this%qflx_irr_demand        (begc:endc))             ; this%qflx_irr_demand      (:)   = nan
-    allocate(this%qflx_lat_aqu           (begc:endc))             ; this%qflx_lat_aqu         (:)   = nan
-    allocate(this%qflx_lat_aqu_layer     (begc:endc,1:nlevgrnd))  ; this%qflx_lat_aqu_layer   (:,:) = nan
+    allocate(this%qflx_lat_aqu           (begc:endc))             ; this%qflx_lat_aqu         (:)   = 0._r8
+    allocate(this%qflx_lat_aqu_layer     (begc:endc,1:nlevgrnd))  ; this%qflx_lat_aqu_layer   (:,:) = 0._r8
     allocate(this%qflx_surf_input        (begc:endc))             ; this%qflx_surf_input         (:)   = nan   
     allocate(this%qflx_tide              (begc:endc))             ; this%qflx_tide            (:)   = nan !TAO
 
@@ -5484,6 +5512,18 @@ contains
     call hist_addfld1d (fname='QFLX_LAT_AQU',  units='mm/s',  &
          avgflag='A', long_name='Lateral flow between hummock and hollow', &
          ptr_col=this%qflx_lat_aqu, c2l_scale_type='urbanf')
+
+   !SLL added 7/27/21
+    this%qflx_lat_aqu_layer(begc:endc, :) = spval
+    call hist_addfld2d (fname='QFLX_LAT_AQU_LAYER',  units='mm/s', type2d='levgrnd', &
+         avgflag='A', long_name='Lateral flow between hummock and hollow by layer', &
+         ptr_col=this%qflx_lat_aqu_layer)
+
+   !SLL added 4/15/21
+   this%qflx_tide(begc:endc) = spval
+    call hist_addfld1d (fname='QFLX_TIDE',  units='mm H2O/s',  &
+         avgflag='A', long_name='Tidal flux between marsh columns', &
+         ptr_col=this%qflx_tide)
 #endif 
 
    this%qflx_adv(begc:endc,:) = spval

components/elm/src/data_types/VegetationDataType.F90
@@ -1976,6 +1976,7 @@ module VegetationDataType
        allocate(this%grainc_xfer        (begp :endp))   ;  this%grainc_xfer        (:)   = nan
        allocate(this%woodc              (begp :endp))   ;  this%woodc              (:)   = nan
        allocate(this%totvegc_abg        (begp :endp))   ;  this%totvegc_abg        (:)   = nan
+     ! allocate(this%osm_inhib          (begp :endp))   ;  this%osm_inhib          (:)   = nan
     endif  !  not use_fates
 
     allocate(this%begcb              (begp :endp))   ;  this%begcb              (:) = nan
@@ -2148,6 +2149,11 @@ module VegetationDataType
             avgflag='A', long_name='total aboveground vegetation carbon, excluding cpool', &
             ptr_patch=this%totvegc_abg)
 
+     !   this%osm_inhib(begc:endc) = spval
+     !   call hist_addfld1d (fname='OSM_INHIB',  units=' ',  &
+     !        avgflag='A', long_name='Factor to reduce growth due to salinity stress', &
+     !        ptr_col=this%osm_inhib)
+            
        ! end of c12 block
 
     else if ( carbon_type == 'c13' ) then

components/elm/src/data_types/VegetationPropertiesType.F90
@@ -155,6 +155,13 @@ module VegetationPropertiesType
      integer, allocatable :: nonvascular(:)       ! nonvascular plant lifeform flag (0 or 1-moss or 2-lichen)
      integer, allocatable :: nfixer(:)            ! N-fixer flag (0 or 1)
      !----------------------F.-M. Yuan (2018-03-23): user-defined parameter file ---------------------------------------------------------------------
+     !salinity response parameters
+     real(r8), allocatable :: sal_threshold(:)       !Threshold for salinity effects (ppt)
+     real(r8), allocatable :: KM_salinity(:)         !Half saturation constant for omotic inhibition function (ppt)
+     real(r8), allocatable :: osm_inhib(:)           !Osmotic inhibition factor
+     real(r8), allocatable :: sal_opt(:)             !Salinity at which optimal biomass occurs (ppt)
+     real(r8), allocatable :: sal_tol(:)             !Salinity tolerance; width parameter for Gaussian distribution (ppt -1)
+     !real(r8), allocatable :: floodf(:)              !Growth inhibition factor due to flooding/inundation (0-1)
 
    contains
    procedure, public :: Init => veg_vp_init
@@ -193,6 +200,7 @@ contains
     use pftvarcon , only : fnr, act25, kcha, koha, cpha, vcmaxha, jmaxha, tpuha
     use pftvarcon , only : lmrha, vcmaxhd, jmaxhd, tpuhd, lmrse, qe, theta_cj
     use pftvarcon , only : bbbopt, mbbopt, nstor, br_xr, tc_stress, lmrhd, crit_gdd1, crit_gdd2
+    use pftvarcon , only : sal_threshold, KM_salinity, osm_inhib, sal_opt, sal_tol !floodf
     !
     !----------------------F.-M. Yuan (2018-03-23): user-defined parameter file ---------------------------------------------------------------------
     use pftvarcon , only : nonvascular, nfixer
@@ -327,12 +335,16 @@ contains
 
     allocate( this%crit_gdd1(0:numpft))                          ; this%crit_gdd1(:)             =nan
     allocate( this%crit_gdd2(0:numpft))                          ; this%crit_gdd2(:)             =nan
-
     !----------------------F.-M. Yuan (2018-03-23): user-defined parameter file ---------------------------------------------------------------------
     allocate(this%nonvascular(0:numpft))                         ; this%nonvascular(:)           =huge(1)
     allocate(this%nfixer(0:numpft))                              ; this%nfixer(:)                =huge(1)
-    !----------------------F.-M. Yuan (2018-03-23): user-defined parameter file ---------------------------------------------------------------------
-
+ 
+    allocate( this%sal_threshold(0:numpft))        ; this%sal_threshold(:)       =nan
+    allocate( this%KM_salinity(0:numpft))          ; this%KM_salinity(:)         =nan
+    allocate( this%osm_inhib(0:numpft))            ; this%osm_inhib(:)           =nan
+    allocate( this%sal_opt(0:numpft))              ; this%sal_opt(:)             =nan
+    allocate( this%sal_tol(0:numpft))              ; this%sal_tol(:)             =nan   
+    !allocate( this%floodf(0:numpft))               ; this%floodf(:)              =nan
     do m = 0,numpft
 
        if (m <= ntree) then
@@ -451,6 +463,12 @@ contains
         this%vmax_nfix(m)      = vmax_nfix(m)
         this%km_nfix(m)        = km_nfix(m)
         this%vmax_ptase(m)     = vmax_ptase(m)
+        this%sal_threshold(m)  = sal_threshold(m)
+        this%KM_salinity(m)    = KM_salinity(m)
+        this%osm_inhib(m)      = osm_inhib(m)
+        this%sal_opt(m)        = sal_opt(m)
+        this%sal_tol(m)        = sal_tol(m)
+        !this%floodf(m)         = floodf(m)
 
         do j = 1 , nlevdecomp
            this%decompmicc_patch_vr(m,j) = decompmicc_patch_vr(j,m)

ORNLngee/E3SM_tai

components/elm/src/data_types/ColumnDataType.F90
@@ -112,6 +112,8 @@ module ColumnDataType
     real(r8), pointer :: h2osoi_vol         (:,:) => null() ! volumetric soil water (0<=h2osoi_vol<=watsat) (1:nlevgrnd) (m3/m3)
     real(r8), pointer :: h2osfc             (:)   => null() ! surface water (kg/m2)
     real(r8), pointer :: salinity           (:)   => null() ! salinity from PFLOTRAN when using interface (TAO 5/19/2020)
+    real(r8), pointer :: salt_content       (:,:) => null() ! salt mass for each soil layer
+    real(r8), pointer :: floodf             (:)   => null() ! Flood factor to reduce growth when plants submerged
     real(r8), pointer :: h2ocan             (:)   => null() ! canopy water integrated to column (kg/m2)
     real(r8), pointer :: total_plant_stored_h2o(:)=> null() ! total water in plants (kg/m2)
     real(r8), pointer :: wslake_col         (:)   => null() ! col lake water storage (mm H2O)
@@ -432,6 +434,7 @@ module ColumnDataType
     real(r8), pointer :: eflx_hs_top_snow        (:)   => null() ! heat flux on top snow layer (W/m2)
     real(r8), pointer :: eflx_hs_soil            (:)   => null() ! heat flux on soil [W/m2
     real(r8), pointer :: eflx_sabg_lyr           (:,:) => null() ! absorbed solar radiation (col,lyr) (W/m2)
+    real(r8), pointer :: eflx_sh_tide            (:)   => null() ! sensible heat flux from tide
     ! Derivatives of energy fluxes
     real(r8), pointer :: eflx_dhsdT              (:)   => null() ! deriv. of energy flux into surface layer wrt temp (W/m2/K)
     ! Latent heat terms
@@ -1422,6 +1425,8 @@ contains
     allocate(this%bw                 (begc:endc,-nlevsno+1:0))        ; this%bw                 (:,:) = spval
     allocate(this%smp_l              (begc:endc,-nlevsno+1:nlevgrnd)) ; this%smp_l              (:,:) = spval
     allocate(this%salinity           (begc:endc))                     ; this%salinity           (:)   = spval
+    allocate(this%salt_content       (begc:endc,1:nlevgrnd))          ; this%salt_content       (:,:) = spval
+    allocate(this%floodf             (begc:endc))                     ; this%floodf             (:)   = spval
     allocate(this%soilp              (begc:endc,1:nlevgrnd))          ; this%soilp              (:,:) = 0._r8
     allocate(this%swe_old            (begc:endc,-nlevsno+1:0))        ; this%swe_old            (:,:) = spval
     allocate(this%snw_rds            (begc:endc,-nlevsno+1:0))        ; this%snw_rds            (:,:) = spval
@@ -1544,6 +1549,21 @@ contains
           avgflag='A', long_name='surface water depth', &
            ptr_col=this%h2osfc)
 
+   this%salinity(begc:endc) = spval
+    call hist_addfld1d (fname='SALINITY',  units='ppt', &
+         avgflag='A', long_name='Salinity concentration', &
+         ptr_col=this%salinity)
+
+   this%salt_content(begc:endc,:) = spval
+    call hist_addfld2d (fname='SALT_CONTENT',  units='g', type2d='levgrnd', &
+         avgflag='A', long_name='Mass of salt in soil layer', &
+         ptr_col=this%salt_content)
+   
+   this%floodf(begc:endc) = spval
+    call hist_addfld1d (fname='FLOODF',  units='', &
+         avgflag='A', long_name='Factor 0-1 to reduce plant growth due to flooding', &
+         ptr_col=this%floodf)
+   
     this%h2osoi_vol(begc:endc,:) = spval
      call hist_addfld2d (fname='H2OSOI',  units='mm3/mm3', type2d='levgrnd', &
           avgflag='A', long_name='volumetric soil water (vegetated landunits only)', &
@@ -1695,6 +1715,8 @@ contains
        this%h2osfc(c)                 = 0._r8
        this%h2ocan(c)                 = 0._r8
        this%salinity(c)               = 0._r8
+       this%salt_content(c,:)         = 0._r8
+       this%floodf(c)                 = 0._r8
        this%frac_h2osfc(c)            = 0._r8
        this%frac_h2osfc_act(c)        = 0._r8
        this%h2orof(c)                 = 0._r8
@@ -5648,6 +5670,7 @@ contains
     allocate(this%htvp                 (begc:endc))              ; this%htvp                 (:)   = spval
     allocate(this%xmf                  (begc:endc))              ; this%xmf                  (:)   = spval
     allocate(this%xmf_h2osfc           (begc:endc))              ; this%xmf_h2osfc           (:)   = spval
+    allocate(this%eflx_sh_tide         (begc:endc))              ; this%eflx_sh_tide         (:)   = spval
     allocate(this%imelt                (begc:endc,-nlevsno+1:nlevgrnd))  ; this%imelt        (:,:) = huge(1)
     allocate(this%eflx_soil_grnd       (begc:endc))              ; this%eflx_soil_grnd       (:)   = spval
     allocate(this%eflx_rnet_soil       (begc:endc))              ; this%eflx_rnet_soil       (:)   = spval
@@ -5911,6 +5934,16 @@ contains
     call hist_addfld1d (fname='QFLX_LAT_AQU',  units='mm/s',  &
          avgflag='A', long_name='Lateral flow between hummock and hollow', &
          ptr_col=this%qflx_lat_aqu, c2l_scale_type='urbanf')
+
+    this%qflx_lat_aqu_layer(begc:endc, :) = spval
+    call hist_addfld2d (fname='QFLX_LAT_AQU_LAYER',  units='mm/s', type2d='levgrnd', &
+         avgflag='A', long_name='Lateral flow between hummock and hollow by layer', &
+         ptr_col=this%qflx_lat_aqu_layer)
+
+   this%qflx_tide(begc:endc) = spval
+    call hist_addfld1d (fname='QFLX_TIDE',  units='mm H2O/s',  &
+         avgflag='A', long_name='Tidal flux between marsh columns', &
+         ptr_col=this%qflx_tide)
 
    this%qflx_adv(begc:endc,:) = spval
    call hist_addfld2d (fname='QFLX_ADV',  units='mm/s', type2d='levgrnd', &

components/elm/src/data_types/VegetationDataType.F90
@@ -2007,6 +2007,7 @@ module VegetationDataType
        allocate(this%grainc_xfer        (begp :endp))   ;  this%grainc_xfer        (:)   = spval
        allocate(this%woodc              (begp :endp))   ;  this%woodc              (:)   = spval
        allocate(this%totvegc_abg        (begp :endp))   ;  this%totvegc_abg        (:)   = spval
+       !allocate(this%osm_inhib          (begp :endp))   ;  this%osm_inhib          (:)   = spval
     endif  !  not use_fates
 
     allocate(this%begcb              (begp :endp))   ;  this%begcb              (:) = spval
@@ -2179,6 +2180,11 @@ module VegetationDataType
             avgflag='A', long_name='total aboveground vegetation carbon, excluding cpool', &
             ptr_patch=this%totvegc_abg)
 
+     ! this%osm_inhib(begc:endc) = spval
+     ! call hist_addfld1d (fname='OSM_INHIB',  units=' ',  &
+     !      avgflag='A', long_name='Factor to reduce growth due to salinity stress', &
+     !      ptr_col=this%osm_inhib)
+            
        ! end of c12 block
 
     else if ( carbon_type == 'c13' ) then

components/elm/src/data_types/VegetationPropertiesType.F90
@@ -158,6 +158,12 @@ module VegetationPropertiesType
      real(r8), pointer :: vegshape(:)         ! shape parameter to modify shrub burial by snow (1 = parabolic, 2 = hemispheric)
      real(r8), pointer :: stocking(:)         ! stocking density for pft (stems / hectare)
      real(r8), pointer :: taper(:)            ! ratio of height:radius_breast_height (woody vegetation allometry)
+     !salinity response parameters
+     real(r8), allocatable :: sal_threshold(:)       !Threshold for salinity effects (ppt)
+     real(r8), allocatable :: KM_salinity(:)         !Half saturation constant for omotic inhibition function (ppt)
+     real(r8), allocatable :: osm_inhib(:)           !Osmotic inhibition factor
+     real(r8), allocatable :: sal_opt(:)             !Salinity at which optimal biomass occurs (ppt)
+     real(r8), allocatable :: sal_tol(:)             !Salinity tolerance; width parameter for Gaussian distribution (ppt -1)
 
    contains
    procedure, public :: Init => veg_vp_init
@@ -200,6 +206,7 @@ contains
     ! snow/vegetation interactions (NGEE Arctic IM3)
     use pftvarcon , only : bendresist, stocking, vegshape, taper
     use pftvarcon , only : crit_gdd1, crit_gdd2
+    use pftvarcon , only : sal_threshold, KM_salinity, osm_inhib, sal_opt, sal_tol
     !
 
     class (vegetation_properties_type) :: this
@@ -340,6 +347,12 @@ contains
 
     allocate( this%crit_gdd1(0:numpft))                          ; this%crit_gdd1(:)             =spval
     allocate( this%crit_gdd2(0:numpft))                          ; this%crit_gdd2(:)             =spval
+ 
+    allocate( this%sal_threshold(0:numpft))                      ; this%sal_threshold(:)         =spval
+    allocate( this%KM_salinity(0:numpft))                        ; this%KM_salinity(:)           =spval
+    allocate( this%osm_inhib(0:numpft))                          ; this%osm_inhib(:)             =spval
+    allocate( this%sal_opt(0:numpft))                            ; this%sal_opt(:)               =spval
+    allocate( this%sal_tol(0:numpft))                            ; this%sal_tol(:)               =spval
     do m = 0,numpft
 
        ! not needed anymore: woody(m)=1 for tree, 2 for shrub, or 0 for any other
@@ -456,6 +469,11 @@ contains
         this%vmax_nfix(m)      = vmax_nfix(m)
         this%km_nfix(m)        = km_nfix(m)
         this%vmax_ptase(m)     = vmax_ptase(m)
+        this%sal_threshold(m)  = sal_threshold(m)
+        this%KM_salinity(m)    = KM_salinity(m)
+        this%osm_inhib(m)      = osm_inhib(m)
+        this%sal_opt(m)        = sal_opt(m)
+        this%sal_tol(m)        = sal_tol(m)
 
         do j = 1 , nlevdecomp
            this%decompmicc_patch_vr(m,j) = decompmicc_patch_vr(j,m)