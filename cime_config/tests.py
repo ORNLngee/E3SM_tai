ORNLngee/E3SM_ORNL_IM

Add a few tests for local ELM testing: (1) tests for important ELM modules or functions; (2) 2 debug tests; (3) a long-term test for FLUXNET 42 sites.

cime_config/tests.py
@@ -80,6 +80,31 @@ _TESTS = {
             )
         },
 
+    "e3sm_land_mydebugs" : {
+        "share" : True,
+        "time"  : "0:45:00",
+        "tests" : (
+            "SMS_Ly2_P1x1_D.1x1_smallvilleIA.IELMCNCROP.elm-lulcc_sville",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-mydebug_I1850CNPRDCTCBC"
+            )
+        },
+
+    "e3sm_land_mytests" : {
+        "share" : True,
+        "time"  : "0:45:00",
+        "tests" : (
+            "SMS_Ly2_P1x1.1x1_smallvilleIA.IELMCNCROP.elm-per_crop",
+            "SMS_Ly2_P1x1_D.1x1_smallvilleIA.IELMCNCROP.elm-lulcc_sville",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrdat_I1850CNPRDCTCBC42sites",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrdat_I1850CNPRDCTCBCxcol2",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrdat_I1850CNPRDCTCBCxcol3",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_arctic_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_onsetgdd_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_codetest_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_default_I1850CNPRDCTCBC"
+            )
+        },
+
     "e3sm_land_developer" : {
         "share" : True,
         "time"  : "0:45:00",
@@ -98,6 +123,7 @@ _TESTS = {
             "SMS.r05_r05.IELM.elm-topounit",
             "SMS.r05_r05.IELM.elm-topounit_im2",
             "ERS.ELM_USRDAT.I1850ELM.elm-usrdat",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrdat_I1850CNPRDCTCBC42sites",
             "ERS.r05_r05.IELM.elm-lnd_rof_2way",
             "ERS.r05_r05.IELM.elm-V2_ELM_MOSART_features",
             "ERS.ELM_USRDAT.IELM.elm-surface_water_dynamics"

ORNLngee/E3SM_tai

cime_config/tests.py
@@ -80,6 +80,29 @@ _TESTS = {
             )
         },
 
+    "e3sm_land_mydebugs" : {
+        "share" : True,
+        "time"  : "0:45:00",
+        "tests" : (
+            "SMS_Ly2_P1x1_D.1x1_smallvilleIA.IELMCNCROP.elm-lulcc_sville",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-mydebug_I1850CNPRDCTCBC"
+            )
+        },
+
+    "e3sm_land_mytests" : {
+        "share" : True,
+        "time"  : "0:45:00",
+        "tests" : (
+            "SMS_Ly2_P1x1.1x1_smallvilleIA.IELMCNCROP.elm-per_crop",
+            "SMS_Ly2_P1x1_D.1x1_smallvilleIA.IELMCNCROP.elm-lulcc_sville",
+            "ERS_Ly10.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrdat_I1850CNPRDCTCBC42sites",
+            "ERS_Ly10.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_arctic_I1850CNPRDCTCBC",
+            "ERS_Ly10.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_onsetgdd_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_codetest_I1850CNPRDCTCBC",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrpft_default_I1850CNPRDCTCBC"
+            )
+        },
+
     "e3sm_land_developer" : {
         "share" : True,
         "time"  : "0:45:00",
@@ -98,6 +121,7 @@ _TESTS = {
             "SMS.r05_r05.IELM.elm-topounit",
             "SMS.r05_r05.IELM.elm-topounit_im2",
             "ERS.ELM_USRDAT.I1850ELM.elm-usrdat",
+            "ERS.ELM_USRDAT.I1850CNPRDCTCBC.elm-usrdat_I1850CNPRDCTCBC42sites",
             "ERS.r05_r05.IELM.elm-lnd_rof_2way",
             "ERS.r05_r05.IELM.elm-V2_ELM_MOSART_features",
             "ERS.ELM_USRDAT.IELM.elm-surface_water_dynamics"